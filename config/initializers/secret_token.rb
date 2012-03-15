e8666b19ef3a1ef8ddb7fe40bec84305ed14b223
Yvan MBAPPOU <yvan.mbappou@enseeiht.fr>
2012-03-15 16:40:13 +0000

    projet créé

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '147b1c459f4266269283c994c5e0e219218cb99e30707d010b849ee9917fd2054b9f88d2b5f1209649a5b0a038e788ce3f3966eeb34e686bc57736ce495f6a3f'
